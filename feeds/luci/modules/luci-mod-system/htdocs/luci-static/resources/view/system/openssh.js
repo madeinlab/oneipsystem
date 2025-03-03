1ddfe496b3c41bf641eae8e5eda8e3cff3c0f06a
yjyoon <yjyoon@madeinlab.co.kr>
2025-03-03 18:00:59 +0900

    openssh Change the OpenSSH page title and menu name (OpenSSH → SSH). Remove unnecessary configuration options. Modify default values (Port: '2222' → '22', PermitRootLogin: 'no' → 'yes').

--- feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/openssh.js
+++ feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/openssh.js
@@ -4,11 +4,16 @@
 'require uci';
 'require ui';
 'require fs';
+'require firewall as fwmodel';
+'require tools.firewall as fwtool';
+
+const SSH_RULE_NAME = 'Allow-SSH'
 
 return view.extend({
     load: function() {
         return Promise.all([
             uci.load('openssh'),
+            uci.load('firewall'),
             fs.exec('/etc/init.d/sshd', ['status'])
         ]).then((results) => {
             let sections = uci.sections('openssh', 'sshd');
@@ -20,23 +25,33 @@ return view.extend({
                 uci.set('openssh', sid, 'PasswordAuthentication', 'yes');
                 return uci.save();
             }
-            return results[1];
+            return results[2];
         });
     },
 
     render: function() {
-        var m = new form.Map('openssh', _('OpenSSH Server Configuration'),
-            _('Configure OpenSSH server settings. Note: This is separate from Dropbear SSH server.'));
+        var m = new form.Map('openssh', _('SSH Configuration'), _('Configure SSH settings.'));
+        m.chain('firewall')
+    
+        let sshRuleSectionId = null
+        let firewallRules = uci.sections('firewall', 'rule')
+        for (let i = 0; i < firewallRules.length; i++) {
+            if (firewallRules[i]['name'] === SSH_RULE_NAME) { // firewallRules[i]['name'], option 'name'
+                sshRuleSectionId = firewallRules[i]['.name'] // firewallRules[i]['.name'], section_id
+                break
+            }
+        }
 
         var s = m.section(form.NamedSection, 'sshd', 'openssh');
         s.anonymous = true;
         s.addremove = false;
 
         // 기본 설정
-        var o = s.option(form.Flag, 'enabled', _('Enable OpenSSH Server'));
+        var o = s.option(form.Flag, 'enabled', _('Enable'));
         o.rmempty = false;
         o.default = '0';  // Disable
         o.write = function(section_id, value) {
+            // openssh
             uci.set('openssh', section_id, 'enabled', value);
             if (value == '1') {
                 fs.exec('/etc/init.d/sshd', ['enable']);
@@ -45,54 +60,76 @@ return view.extend({
                 fs.exec('/etc/init.d/sshd', ['disable']);
                 fs.exec('/etc/init.d/sshd', ['stop']);
             }
+
+            // firewall
+            if (sshRuleSectionId) {
+                uci.set('firewall', sshRuleSectionId, 'enabled', value)
+            }
         };
 
         o = s.option(form.Value, 'Port', _('SSH Port'));
         o.datatype = 'port';
-        o.default = '2222';
+        o.default = '22';
         o.rmempty = false;
+        o.write = function(section_id, value) {
+            // openssh
+            uci.set('openssh', section_id, 'Port', value);
 
-        // 접근 제어 설정
+            // firewall
+            if (sshRuleSectionId) {
+                console.log("여긴 Port - firewall")
+                uci.set('firewall', sshRuleSectionId, 'dest_port', value)
+            }
+        };
+
+        /**
+        // Allow root login
         o = s.option(form.Flag, 'PermitRootLogin', _('Allow root login'));
         o.enabled = 'yes';
         o.disabled = 'no';
         o.default = o.disabled;  // Disable
         o.rmempty = false;
 
+        // Password Authentication
         o = s.option(form.Flag, 'PasswordAuthentication', _('Password authentication'));
         o.enabled = 'yes';
         o.disabled = 'no';
         o.default = o.enabled;  // Enable
         o.rmempty = false;
 
+        // Listen Address
         o = s.option(form.Value, 'ListenAddress', _('Listen Address'));
         o.datatype = 'ipaddr';
         o.placeholder = '0.0.0.0';
         o.default = '0.0.0.0';
         o.rmempty = false;
 
-        // 인증 설정
+        // Public key authentication
         o = s.option(form.Flag, 'PubkeyAuthentication', _('Public key authentication'));
         o.enabled = 'yes';
         o.disabled = 'no';
         o.default = o.enabled;  // Enable
         o.rmempty = false;
 
+        // Authorized keys file
         o = s.option(form.Value, 'AuthorizedKeysFile', _('Authorized keys file'));
         o.default = '.ssh/authorized_keys';
         o.rmempty = false;
 
+        // Use PAM authentication
         o = s.option(form.Flag, 'UsePAM', _('Use PAM authentication'));
         o.enabled = 'yes';
         o.disabled = 'no';
         o.default = o.enabled;  // Enable
         o.rmempty = false;
 
+        // Max authentication tries
         o = s.option(form.Value, 'MaxAuthTries', _('Max authentication tries'));
         o.datatype = 'uinteger';
         o.default = '3';
         o.rmempty = false;
+         */
 
         return m.render();
     }
-}); 
+});
