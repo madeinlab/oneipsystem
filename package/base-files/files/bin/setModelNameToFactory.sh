a047b37522551dccf388b9db0288c1be7cbe5d74
Seoung-Soo, Lee <sslee@madeinlab.co.kr>
2025-05-16 00:00:53 +0900

    Add script file for Model and Serial

--- package/base-files/files/bin/setModelNameToFactory.sh
+++ package/base-files/files/bin/setModelNameToFactory.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 # Set Model Name by mtk_factory_rw.sh
-echo "Setting Model Name"
-/sbin/mtk_factory_rw.sh -w model $model
+echo "Setting Model Name: $1"
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <ModelNameString>"
+    exit 1
+fi
+
+# 문자열을 hex로 변환 (공백으로 구분, 32바이트 미만이면 00으로 패딩)
+model="$1"
+hex=""
+for ((i=0; i<${#model}; i++)); do
+    hex+=$(printf "%02X " "'${model:$i:1}")
+done
+# 패딩 (32바이트)
+for ((i=${#model}; i<32; i++)); do
+    hex+="00 "
+done
+
+# 마지막 공백 제거
+hex=$(echo "$hex" | sed 's/ *$//')
+
+echo "Hex: $hex"
+/sbin/mtk_factory_rw.sh -w model $hex
 
--- package/base-files/files/bin/setSerialNoToFactory.sh
+++ package/base-files/files/bin/setSerialNoToFactory.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 # Set Serial Number by mtk_factory_rw.sh
-echo "Setting Serial Number"
-/sbin/mtk_factory_rw.sh -w serial_no $serial_no
+echo "Setting Serial Number: $1"
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <SerialNumberString>"
+    exit 1
+fi
+
+# 문자열을 hex로 변환 (공백으로 구분, 16바이트 미만이면 00으로 패딩)
+serial="$1"
+hex=""
+for ((i=0; i<${#serial}; i++)); do
+    hex+=$(printf "%02X " "'${serial:$i:1}")
+done
+# 패딩 (16바이트)
+for ((i=${#serial}; i<16; i++)); do
+    hex+="00 "
+done
+
+# 마지막 공백 제거
+hex=$(echo "$hex" | sed 's/ *$//')
+
+echo "Hex: $hex"
+/sbin/mtk_factory_rw.sh -w serial_no $hex
 
