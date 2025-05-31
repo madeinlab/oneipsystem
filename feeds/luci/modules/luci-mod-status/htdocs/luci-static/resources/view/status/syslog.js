1e962a06a66c9a67ed00d70a310779ac3c401769
Seoung-Soo, Lee <sslee@madeinlab.co.kr>
2025-05-31 12:01:02 +0900

    Added system log automatic update logic

--- feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/syslog.js
+++ feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/syslog.js
@@ -2,6 +2,26 @@
 'require view';
 'require fs';
 'require ui';
+'require poll';
+'require dom';
+
+function startPolling(shellcmd, logreadcmd, grepcmd, textarea) {
+	var logCmd = logreadcmd + ' | ' + grepcmd + ' -E "user\\.|daemon\\."' + '|' + grepcmd + ' -v "kernel"';
+
+	var step = function() {
+		return fs.exec_direct(shellcmd, ['-c', logCmd]).then(function(logdata) {
+			if (textarea && logdata != null) {
+				var loglines = logdata.trim().split(/\n/);
+				textarea.value = loglines.join('\n');
+				textarea.rows = loglines.length + 1;
+			}
+		});
+	};
+
+	return step().then(function() {
+		poll.add(step);
+	});
+}
 
 return view.extend({
 	load: function() {
@@ -16,33 +36,43 @@ return view.extend({
 
 			if (!shellcmd || !logreadcmd || !grepcmd) {
 				ui.addNotification(null, E('p', {}, _('Missing required files: /bin/sh, /sbin/logread, or /bin/grep')));
-				return '';
+				return { shellcmd: null, logreadcmd: null, grepcmd: null, logdata: '' };
 			}
 
-			var logCmd = logreadcmd + ' | ' + grepcmd + ' -E "user\\.|daemon\\."' + '|' + grepcmd + ' -v "kernel"';
+			var logCmd1 = logreadcmd + '|' + grepcmd + ' -E "user\\.|daemon\\."';
+			var logCmd2 = grepcmd + ' -v "kernel"' + '|' + grepcmd + ' -v "S95done:"';
+			var logCmd = logCmd1 + '|' + logCmd2;
 
 			return fs.exec_direct(shellcmd, ['-c', logCmd]).catch(function(err) {
 				ui.addNotification(null, E('p', {}, _('Unable to load log data: ' + err.message)));
 				return '';
+			}).then(function(logdata) {
+				return { shellcmd: shellcmd, logreadcmd: logreadcmd, grepcmd: grepcmd, logdata: logdata };
 			});
 		});
 	},
 
-	render: function(logdata) {
-		var loglines = logdata.trim().split(/\n/);
+	render: function(data) {
+		var loglines = (data.logdata || '').trim().split(/\n/);
+		var textarea = E('textarea', {
+			'id': 'syslog',
+			'style': 'font-size:12px',
+			'readonly': 'readonly',
+			'wrap': 'off',
+			'rows': loglines.length + 1
+		}, [ loglines.join('\n') ]);
 
-		return E([], [
+		var container = E('div', { 'id': 'content_syslog' }, [ textarea ]);
+		var root = E([], [
 			E('h2', {}, [ _('System Log') ]),
-			E('div', { 'id': 'content_syslog' }, [
-				E('textarea', {
-					'id': 'syslog',
-					'style': 'font-size:12px',
-					'readonly': 'readonly',
-					'wrap': 'off',
-					'rows': loglines.length + 1
-				}, [ loglines.join('\n') ])
-			])
+			container
 		]);
+
+		if (data.shellcmd && data.logreadcmd && data.grepcmd) {
+			startPolling(data.shellcmd, data.logreadcmd, data.grepcmd, textarea);
+		}
+
+		return root;
 	},
 
 	handleSaveApply: null,
