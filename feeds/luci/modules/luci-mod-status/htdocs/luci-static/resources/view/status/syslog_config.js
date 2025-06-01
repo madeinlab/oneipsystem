0d6ca3af10f3cf4001d9834604446b1b670070fc
Seoung-Soo, Lee <sslee@madeinlab.co.kr>
2025-06-01 23:44:06 +0900

    Fixed view page of syslog configuration

--- feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/syslog_config.js
+++ feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/syslog_config.js
@@ -17,39 +17,27 @@ return view.extend({
     },
 
     render: function(logdata) {
-        var m, s, o;
         var config = this.parseLogrotateConfig(logdata);
 
-        m = new form.Map('system');
-        s = m.section(form.TypedSection, 'logrotate', _('Log Rotation Settings'));
-        s.anonymous = true;
-        s.addremove = false;
+        return E([], [
+            E('h2', {}, [_('Log Rotation Settings')]),
+            E('table', { 'class': 'table' }, [
+                this.renderRow(_('Log File Path'), config.log_file),
+                this.renderRow(_('Size Limit'), config.size),
+                this.renderRow(_('Number of Rotations'), config.rotate),
+                this.renderRow(_('Rotation Directory'), config.olddir),
+                this.renderRow(_('Date Format'), config.dateformat),
+                this.renderRow(_('File Permissions'), config.create),
+                this.renderRow(_('Extra Options'), config.extra)
+            ])
+        ]);
+    },
 
-        o = s.option(form.Value, 'log_file', _('Log File Path'));
-        o.default = config.log_file;
-        o.readonly = true;
-
-        o = s.option(form.Value, 'size', _('Size Limit'));
-        o.default = config.size;
-        o.readonly = true;
-
-        o = s.option(form.Value, 'rotate', _('Number of Rotations'));
-        o.default = config.rotate;
-        o.readonly = true;
-
-        o = s.option(form.Value, 'olddir', _('Rotation Directory'));
-        o.default = config.olddir;
-        o.readonly = true;
-
-        o = s.option(form.Value, 'dateformat', _('Date Format'));
-        o.default = config.dateformat;
-        o.readonly = true;
-
-        o = s.option(form.Value, 'permissions', _('File Permissions'));
-        o.default = config.create;
-        o.readonly = true;
-
-        return m.render();
+    renderRow: function(label, value) {
+        return E('tr', {}, [
+            E('th', { 'style': 'text-align:left;width:200px;' }, [ label ]),
+            E('td', {}, [ value || '-' ])
+        ]);
     },
 
     parseLogrotateConfig: function(config) {
@@ -59,33 +47,29 @@ return view.extend({
             rotate: '',
             olddir: '',
             dateformat: '',
-            create: ''
+            create: '',
+            extra: ''
         };
-
         var lines = config.split('\n');
+        var extras = [];
         for (var i = 0; i < lines.length; i++) {
             var line = lines[i].trim();
-            
-            if (line.startsWith('/')) {
-                result.log_file = line.split('{')[0].trim();
-            }
-            else if (line.startsWith('size')) {
-                result.size = line.split(' ')[1];
-            }
-            else if (line.startsWith('rotate')) {
-                result.rotate = line.split(' ')[1];
-            }
-            else if (line.startsWith('olddir')) {
-                result.olddir = line.split(' ')[1];
-            }
-            else if (line.startsWith('dateformat')) {
-                result.dateformat = line.split(' ')[1];
-            }
-            else if (line.startsWith('create')) {
-                result.create = line.split(' ').slice(1).join(' ');
-            }
+            if (line.startsWith('/')) result.log_file = line.split('{')[0].trim();
+            else if (line.startsWith('size')) result.size = line.split(' ')[1];
+            else if (line.startsWith('rotate')) result.rotate = line.split(' ')[1];
+            else if (line.startsWith('olddir')) result.olddir = line.split(' ')[1];
+            else if (line.startsWith('dateformat')) result.dateformat = line.split(' ')[1];
+            else if (line.startsWith('create')) result.create = line.split(' ').slice(1).join(' ');
+            else if (line && !line.startsWith('{') && !line.startsWith('}')) extras.push(line);
         }
-
+        result.extra = extras.filter(l =>
+            !l.startsWith('copytruncate') &&
+            !l.startsWith('dateext') &&
+            !l.startsWith('missingok') &&
+            !l.startsWith('notifempty') &&
+            !l.startsWith('postrotate') &&
+            !l.startsWith('endscript')
+        ).join(', ');
         return result;
     },
 
