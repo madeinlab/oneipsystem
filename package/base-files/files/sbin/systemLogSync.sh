49c5470da4ce988f6406eb26e82057168d517094
Seoung-Soo, Lee <sslee@madeinlab.co.kr>
2025-06-03 16:01:00 +0900

    Modify logrotate

---
+++ package/base-files/files/sbin/systemLogSync.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SRC=/tmp/system.log
+
+# Check run process
+EXIST_CHECK_LINK=/tmp/running_check_systemLog
+if [ -e $EXIST_CHECK_LINK ]; then
+ exit 0
+else
+ touch ${EXIST_CHECK_LINK}
+fi
+
+/usr/bin/inotifywait -m $SRC -e close_write | while read; do /usr/sbin/logrotate /etc/logrotate.d/system_log; done
+
+echo "SYSTEM LOG ROTATE" > /dev/console
+logger -p "daemon.notice" -t "procd" "- logrotate -"
+
